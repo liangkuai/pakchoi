629777cd41e6332b1fc509ff003ef47976074cf8
liukai <liukai@souche.com>
2019-05-20 15:08:37 +0800

    ioc
    BeanDefinition 中增加 bean 的属性和值。用来在创建 bean 对象之后，再设置 bean 的属性值。

---
+++ pakchoi-ioc/src/main/java/org/luck/pakchoi/beans/PropertyValue.java
@@ -0,0 +1,34 @@
+package org.luck.pakchoi.beans;
+
+/**
+ * bean 的属性
+ *
+ * @author liukai
+ * @date 2019-05-05
+ */
+public class PropertyValue {
+
+    private String name;
+    private Object value;
+
+    public PropertyValue(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
